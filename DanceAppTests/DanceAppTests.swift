21d6a6997fe6c852d7bc257a745ffc75f1f62abc
smtrived <smtrived@ucsd.edu>
2025-01-06 22:55:05 -0800

    Initial Commit

---
+++ DanceAppTests/DanceAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DanceAppTests.swift
+//  DanceAppTests
+//
+//  Created by Saarthak Trivedi on 1/6/25.
+//
+
+import Testing
+@testable import DanceApp
+
+struct DanceAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
